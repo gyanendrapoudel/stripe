a33661668f41b07e2e1940c71c86edcb4aa38028
Gyanendra <gyanen56@gmail.com>
2024-04-22 11:06:13 +1000

    making hamburger menu responsive using javascript

---
+++ data.js
@@ -0,0 +1,25 @@
+ export  const  data = [
+  {
+    level: 'Products',
+    links: [
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Payment' },
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Terminal' },
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Connect' },
+    ],
+  },
+  {
+    level: 'Developer',
+    links: [
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Plugins' },
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Libraries' },
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Billing' },
+    ],
+  },
+  {
+    level: 'Company',
+    links: [
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'About' },
+      { icon: '<i class="fa-solid fa-xmark"></i>', linksName: 'Customers' },
+    ],
+  },
+]
---
+++ index.js
@@ -0,0 +1,46 @@
+import {data} from './data.js'
+console.log(data)
+const services = document.querySelectorAll('.services li')
+
+
+
+const ham = document.querySelector('.ham');
+const aside = document.querySelector('.aside');
+const xmark = document.querySelector('.xmark')
+const subLinks = document.querySelector('.sub-links')
+
+
+
+console.log(subLinks)
+
+ham.addEventListener('click',()=>{
+   aside.classList.add('show')
+})
+xmark.addEventListener('click', () => {
+  aside.classList.remove('show')
+
+})
+
+subLinks.innerHTML=`${data.map((item)=>{
+    return `<div class="level ">
+    ${item.level}
+    <div class="level-container">
+    ${item.links
+      .map((link) => {
+        return `
+        
+            <div class="level-item">
+             <span>${link.icon}</span>
+             <span>${link.linksName}</span>
+             </div>
+           
+           
+      `
+      })
+      
+      .join('')}
+      </div>
+    </div>`
+}).join('')}`
+
+console.log(subLinks.innerHTML)
